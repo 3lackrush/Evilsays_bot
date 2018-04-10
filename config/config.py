96e058d6b8052b9ec8675aa2532f80a430cf65f0
3lackRush <505478266@qq.com>
2018-04-10 22:05:04 +0800

    add configuration file

---
+++ config/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding:utf-8 
+__author__ = 'Kios <root@mkernel.com>'
+__desc__ = 'Telegram Bot: Evilsays_bot | configuration files'
+
+token = "Your Token"
+
+mysql_host = '127.0.0.1'
+mysql_port = 3306
+mysql_username = 'username'
+mysql_password = 'password'
+mysql_db = 'cve'
+mysql_chrset = 'utf8'
+
+update_check_url = "http://cve.mitre.org/data/downloads/index.html"
+update_download_url =  "http://cve.mitre.org/data/downloads/allitems.csv"
+
+
