83305e36cd59d20c357020f5eb280d01438150dd
3lackRush <505478266@qq.com>
2018-04-10 22:11:49 +0800

    add csv updating program

---
+++ update_csv.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding:utf-8
+__author__ = 'Kios <root@mkernel.com>'
+__desc__ = 'Telegram Bot: Evilsays_bot | update csv file'
+__link__ = 'http://cve.mitre.org/data/downloads/index.html'
+
+import requests
+from config.config import update_check_url, update_download_url
+from lxml import html
+from db import CVESQL
+
+class Updator(object):
+
+    def __init__(self):
+        self.url = update_check_url
+        self.downloadLink = update_download_url
+
+    def getUpdateDate(self):
+        r = requests.get(self.url)
+        html_data = r.content
+        html_tree = html.fromstring(html_data)
+        result = html_tree.xpath('//*[@id="CenterPane"]/div[1]')
+        data = (result[0].text.split(":")[1]).split("\n")[1]
+        return data
+
+    def checkUpdated(self):
+        try:
+            cve_web_data = self.getUpdateDate()
+            obj = CVESQL()
+            sql = "SELECT * from cve_update"
+            args = ()
+            res = obj.getbysql(sql, args)
+            db_date = res[0][0]
+            if db_date != cve_web_data:
+                return False
+            else:
+                return True
+        except Exception as e:
+            return False
+
+    def downloadNewCsv(self):
+        r = requests.get(self.downloadLink, stream=True)
+        with open('./data/newCsv.csv', 'wb') as fd:
+            for chunk in r.iter_content(chunk_size = 1024*100):
+                fd.write(chunk)
+        fd.close()
+
+if __name__ == '__main__':
+    obj = Updator()
+    print obj.getUpdateDate()
+    print obj.checkUpdated()
+    obj.downloadNewCsv()
